d49d7185e98f2456de12a799c41db1f9496e43a6
Danil Maiorov <damayorov93@gmail.com>
2022-02-19 02:57:23 +0500

    complete toDo

--- js/script.js
+++ js/script.js
@@ -2,10 +2,23 @@ const todoControl = document.querySelector('.todo-control');
 const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
-
+const empty = document.querySelector('.empty-message')
 let toDoData = [];
+console.log();
 
-const render = function () {
+function animate ({timing, draw, duration}) {
+    let start = performance.now();
+    requestAnimationFrame(function animate(time) {
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+        let progress = timing(timeFraction)
+        draw(progress);
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+    });
+}
+function render () {
     todoList.innerHTML = '';
     todoCompleted.innerHTML = '';
     toDoData.forEach(function(item, index){
@@ -16,9 +29,7 @@ const render = function () {
             '<button class="todo-remove"></button>' + 
             '<button class="todo-complete"></button>' + 
             '</div>';
-
             localStorage.setItem('toDoData', JSON.stringify(toDoData));
-
             if (item.completed) {
                 todoCompleted.append(li);
             } else {
@@ -30,11 +41,51 @@ const render = function () {
             });
             li.querySelector('.todo-remove').addEventListener('click', function(){
                 toDoData.splice(index, 1);
-                localStorage.removeItem('toDoData') 
+                localStorage.removeItem('toDoData'); 
                 render();
             });
     });   
 };
+function reload () {
+    if (localStorage.key('toDoData')) {
+        toDoData = JSON.parse(localStorage.getItem('toDoData'));
+        render();
+    }
+}
+function showMessage () {
+    animate({
+        duration: 500,
+        timing(timeFraction) {
+        return timeFraction;
+        },
+        draw(progress) {
+            empty.style.display = 'flex'
+            empty.style.opacity = progress; 
+            headerInput.parentNode.parentNode.parentNode.style.background = '#FFDEEF'
+        }
+    });
+}
+function hideMessage() {
+    animate({
+        duration: 500,
+        timing(timeFraction) {
+            return timeFraction;
+        },
+        draw(progress) {
+            empty.style.opacity = 1 - progress;
+            headerInput.parentNode.parentNode.parentNode.style.background = '#E4EFAF'
+            setTimeout(() => {
+                empty.style.display = 'none';
+            }, 500);
+        }
+    })
+}
+headerInput.addEventListener('input', () => {
+    hideMessage()
+})
+
+todoList.innerHTML = '';
+todoCompleted.innerHTML = '';
 
 todoControl.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -43,17 +94,13 @@ todoControl.addEventListener('submit', function(event) {
         completed: false,
     }
     if (headerInput.value.trim() === ''){
-        alert ('Нужно бы ввести имя задачки');
+       showMessage()
     } else {
         toDoData.push(newToDo);
+        hideMessage()
     }
-        headerInput.value = '';
+        todoControl.reset()
         render();
 });
-function reload() {
-    if (localStorage.key('toDoData')) {
-        toDoData = JSON.parse(localStorage.getItem('toDoData'));
-        render ();
-    }
-}
+
 reload();
